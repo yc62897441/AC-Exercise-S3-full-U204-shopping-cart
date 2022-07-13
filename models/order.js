485d6511c4cf18841a38df4df4d122a10cdea6e4
yc62897441 <yc62897441@gmail.com>
2022-07-13 14:00:05 +0800

    定義資料表、model間關係

---
+++ models/order.js
@@ -0,0 +1,37 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Order extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Order.belongsTo(models.User)
+      Order.hasMany(models.Payment)
+      Order.belongsToMany(models.Product, {
+        through: models.OrderItem,
+        foreignerKey: 'OrderId',
+        as: 'items'
+      })
+    }
+  }
+  Order.init({
+    name: DataTypes.STRING,
+    phone: DataTypes.STRING,
+    address: DataTypes.STRING,
+    amount: DataTypes.INTEGER,
+    sn: DataTypes.INTEGER,
+    shipping_status: DataTypes.STRING,
+    payment_status: DataTypes.STRING,
+    UserId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Order',
+  });
+  return Order;
+};
---
+++ models/product.js
@@ -0,0 +1,36 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Product extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Product.belongsToMany(models.Order, {
+        through: models.OrderItem,
+        foreignerKey: 'ProductId',
+        as: 'orders'
+      })
+      Product.belongsToMany(models.Cart, {
+        through: models.CartItem,
+        foreignerKey: 'ProductId',
+        as: 'carts'
+      })
+    }
+  }
+  Product.init({
+    name: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    price: DataTypes.INTEGER,
+    image: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Product',
+  });
+  return Product;
+};
