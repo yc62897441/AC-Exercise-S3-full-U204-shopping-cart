056c2169cbd16ddfee1f67b43aefc1dad38715bf
yc62897441 <yc62897441@gmail.com>
2022-07-15 10:13:28 +0800

    實作購物車操作: POST cartItem/add、cartItem/sub、DELETE /cartItem

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -10,13 +10,16 @@ const cartController = {
       .then(cart => {
         cart = cart || { items: [] }
         let totalPrice = cart.items.length > 0 ? cart.items.map(d => d.price * d.CartItem.quantity).reduce((a, b) => a + b) : 0
-        return res.render('cart', { cart: cart.toJSON(), totalPrice })
+        if (cart.items.length > 0) {
+          cart = cart.toJSON()
+        }
+        return res.render('cart', { cart: cart, totalPrice })
       })
   },
   postCart: (req, res) => {
     return Cart.findOrCreate({ where: { id: req.session.cartId || 0 } })
       .then(function ([cart, created]) {
-        return CartItem.findOrCreate({ where: { CartId: cart.id, ProductId: req.body.productId },default: { CartId: cart.id, ProductId: req.body.productId } })
+        return CartItem.findOrCreate({ where: { CartId: cart.id, ProductId: req.body.productId }, default: { CartId: cart.id, ProductId: req.body.productId } })
           .then(function ([cartItem, created]) {
             return cartItem.update({ quantity: (cartItem.quantity || 0) + 1 })
               .then((cartItem) => {
@@ -25,6 +28,37 @@ const cartController = {
                   return res.redirect('back')
                 })
               })
+          })
+      })
+  },
+  addCartItem: (req, res) => {
+    return CartItem.findByPk(req.params.id)
+      .then(cartItem => {
+        cartItem.update({
+          quantity: cartItem.quantity + 1
+        })
+          .then(cartItem => {
+            return res.redirect('back')
+          })
+      })
+  },
+  subCartItem: (req, res) => {
+    return CartItem.findByPk(req.params.id)
+      .then(cartItem => {
+        cartItem.update({
+          quantity: cartItem.quantity - 1 >= 1 ? cartItem.quantity - 1 : 1
+        })
+          .then(cartItem => {
+            return res.redirect('back')
+          })
+      })
+  },
+  deleteCartItem: (req, res) => {
+    CartItem.findByPk(req.params.id)
+      .then(cartItem => {
+        cartItem.desdroy()
+          .then(cartItem => {
+            return res.redirect('back')
           })
       })
   }
--- controllers/productController.js
+++ controllers/productController.js
@@ -1,12 +1,24 @@
 const db = require('../models')
 const Product = db.Product
+const Cart = db.Cart
 const PAGE_LIMIT = 3;
 const PAGE_OFFSET = 0;
 
 const productController = {
   getProducts: (req, res) => {
     return Product.findAndCountAll({ offset: PAGE_OFFSET, limit: PAGE_LIMIT, raw: true, nest: true })
-      .then(products => { return res.render('products', { products }) })
+      .then(products => {
+        Cart.findByPk(req.session.cartId, { include: 'items' })
+          .then(cart => {
+            cart = cart || { items: [] }
+            let totalPrice = 0
+            if (cart.items.length > 0) {
+              cart = cart.toJSON()
+              totalPrice = cart.items.map(d => d.price * d.CartItem.quantity).reduce((a, b) => a + b)
+            }
+            return res.render('products', { products, cart, totalPrice })
+          })
+      })
   }
 }
 
