a45e0c0a481915bd3f960e442df6641e04ce2796
yc62897441 <yc62897441@gmail.com>
2022-07-15 11:17:37 +0800

    實作訂單操作: Post order、POST order/cancel

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -9,12 +9,59 @@ const orderController = {
       .then(orders => {
         orders = orders.map(order => ({
           ...order.dataValues,
-          items: order.items.map(item => ({ 
+          items: order.items.map(item => ({
             ...item.dataValues,
-            OrderItem: { ...item.OrderItem.dataValues}
-           }))
+            OrderItem: { ...item.OrderItem.dataValues }
+          }))
         }))
         return res.render('orders', { orders })
+      })
+  },
+  postOrder: (req, res) => {
+    Cart.findByPk(req.session.cartId, { include: 'items' })
+      .then(cart => {
+        if (cart.items.length > 0) {
+          cart = cart.toJSON()
+        }
+
+        Order.create({
+          name: req.body.name,
+          address: req.body.address,
+          phone: req.body.phone,
+          shipping_status: req.body.shipping_status,
+          payment_status: req.body.payment_status,
+          amount: req.body.amount,
+        })
+          .then(order => {
+            let results = []
+            for (let i = 0; i < cart.items.length; i++) {
+              results.push(
+                OrderItem.create({
+                  OrderId: order.id,
+                  ProductId: cart.items[i].id,
+                  price: cart.items[i].price,
+                  quantity: cart.items[i].CartItem.quantity,
+                })
+              )
+            }
+            return Promise.all(results)
+              .then(() => {
+                res.redirect('/orders')
+              })
+          })
+      })
+  },
+  cancelOrder: (req, res) => {
+    Order.findByPk(req.params.id)
+      .then(order => {
+        order.update({
+          ...req.body,
+          shipping_status: '-1',
+          payment_status: '-1',
+        })
+          .then(order => {
+            return res.redirect('back')
+          })
       })
   }
 }
