545f639acb52db43f9a1a7c5fa92e352f9d4a41b
yc62897441 <yc62897441@gmail.com>
2022-07-17 11:45:36 +0800

    串接金流 (1): GET /order/:id/payment，尚未刪除console.log()

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,8 +1,98 @@
 const nodemailer = require('nodemailer')
+const crypto = require('crypto')
 const db = require('../models')
 const Order = db.Order
 const OrderItem = db.OrderItem
 const Cart = db.Cart
+
+
+const URL = ''
+const MerchantID = process.env.MerchantID
+const HashKey = process.env.HashKey
+const HashIV = process.env.HashIV
+const PayGateWay = "https://ccore.spgateway.com/MPG/mpg_gateway"
+const ReturnURL = URL + "/spgateway/callback?from=ReturnURL"
+const NotifyURL = URL + "/spgateway/callback?from=NotifyURL"
+const ClientBackURL = URL + "/orders"
+
+const testData = {
+  name: 'John',
+  wife: 'Mary'
+}
+
+function getTradeInfo(Amt, Desc, email) {
+  data = {
+    'MerchantID': MerchantID, // 商店代號
+    'RespondType': 'JSON', // 回傳格式
+    'TimeStamp': Date.now(), // 時間戳記
+    'Version': 2.0, // 串接程式版本
+    'MerchantOrderNo': Date.now(), // 商店訂單編號
+    'LoginType': 0, // 智付通會員
+    'OrderComment': 'OrderComment', // 商店備註
+    'Amt': Amt, // 訂單金額
+    'ItemDesc': Desc, // 產品名稱
+    'Email': email, // 付款人電子信箱
+    'ReturnURL': ReturnURL, // 支付完成返回商店網址
+    'NotifyURL': NotifyURL, // 支付通知網址/每期授權結果通知
+    'ClientBackURL': ClientBackURL, // 支付取消返回商店網址
+  }
+
+  const chainedData = getDataChain(data)
+  console.log('chainedData', chainedData)
+  console.log('==========')
+  console.log('==========')
+  const mpg_aes_encrypt = create_mpg_aes_encrypt(chainedData)
+  console.log('mpg_aes_encrypt', mpg_aes_encrypt)
+  console.log('==========')
+  console.log('==========')
+  const mpg_sha_encrypt = create_mpg_sha_encrypt(mpg_aes_encrypt)
+  console.log('mpg_sha_encrypt', mpg_sha_encrypt)
+  console.log('==========')
+  console.log('==========')
+
+  tradeInfo = {
+    'MerchantID': MerchantID, // 商店代號
+    'TradeInfo': mpg_aes_encrypt, // 加密後參數
+    'TradeSha': mpg_sha_encrypt,
+    'Version': 2.0, // 串接程式版本
+    'PayGateWay': PayGateWay,
+    'MerchantOrderNo': data.MerchantOrderNo,
+  }
+
+  console.log('tradeInfo', tradeInfo)
+  console.log('==========')
+  console.log('==========')
+  return tradeInfo
+}
+
+function getDataChain(TradeInfo) {
+  let results = []
+  for (let [key, value] of Object.entries(TradeInfo)) {
+    results.push(`${key}=${value}`)
+  }
+  const chainedData = results.join('&')
+  return chainedData
+}
+
+function create_mpg_aes_encrypt(chainedData) {
+  let encrypt = crypto.createCipheriv("aes256", HashKey, HashIV)
+  console.log('encrypt', encrypt)
+  console.log('====')
+  console.log('====')
+  let enc = encrypt.update(chainedData, "utf8", "hex")
+  console.log('enc', enc)
+  console.log('====')
+  console.log('====')
+  let mpg_aes_encrypt = enc + encrypt.final("hex")
+  return mpg_aes_encrypt
+}
+
+function create_mpg_sha_encrypt(mpg_aes_encrypt) {
+  let sha = crypto.createHash('sha256')
+  let plainText = `HashKey=${HashKey}&${mpg_aes_encrypt}&HashIV=${HashIV}`
+  const mpg_sha_encrypt = sha.update(plainText).digest('hex').toUpperCase()
+  return mpg_sha_encrypt
+}
 
 // Gmail 自 2022/5/30 停用低安全性應用程式存取權
 // 還未找出解方，以 nodemailer 使用 gmail
@@ -118,11 +208,23 @@ const orderController = {
       })
   },
   getPayment: (req, res) => {
+    const Amt = 10000 
+    const Desc = '測試'
+    const email = 'abc@abc.com'
+    const TradeInfo = getTradeInfo(Amt, Desc, email)
+    console.log('**************')
+    console.log('**************')
+    console.log('TradeInfo', TradeInfo)
+    console.log('**************')
+    console.log('**************')
+
     return Order.findByPk(req.params.id)
       .then(order => {
         order = order.toJSON()
-
-        return res.render('payment', { order })
+        console.log('TradeInfo', TradeInfo)
+        console.log('**************')
+        console.log('**************')
+        return res.render('payment', { order, tradeInfo: TradeInfo })
       })
   },
   spgatewayCallback: (req, res) => {
@@ -131,7 +233,7 @@ const orderController = {
     console.log(req.body)
     console.log('==========')
     console.log('==========')
-    return res.redirect('back')
+    return res.redirect('/orders')
   }
 }
 
